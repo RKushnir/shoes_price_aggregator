d16d47d3103ec55140c3a5555e5e7241778cf89a
Roman <broilerster@gmail.com>
2013-12-18 23:28:27 +0200

    Specify browser-like user-agent

--- lib/product_fetcher.rb
+++ lib/product_fetcher.rb
@@ -23,6 +23,8 @@ class ProductFetcher
       faraday.adapter :typhoeus
     end
 
+    connection.headers[:user_agent] = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0"
+
     response = connection.get('/master')
     first_page = Nokogiri::HTML(response.body)
     last_paging_anchor = first_page.css('.paging a:not(.arr)').last
